f9fc94fc97ea130245dc8fe45571fe6e140588d5
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-02-29 15:32:54 +0900

    adjust mine delay by range and count of blocks to mine

--- src/main/java/io/github/yuko1101/digitalmineradjust/DigitalMinerAdjust.java
+++ src/main/java/io/github/yuko1101/digitalmineradjust/DigitalMinerAdjust.java
@@ -1,24 +1,11 @@
 package io.github.yuko1101.digitalmineradjust;
 
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.ArrayList;
 
 @Mod(DigitalMinerAdjust.MOD_ID)
 public class DigitalMinerAdjust {
     public static final String MOD_ID = "digitalmineradjust";
 
-    public static final ArrayList<TagKey<Block>> disallowedBlocks = new ArrayList<>();
-
-    static {
-        disallowedBlocks.add(Tags.Blocks.ORES);
-        disallowedBlocks.add(Tags.Blocks.NEEDS_NETHERITE_TOOL);
-        disallowedBlocks.add(Tags.Blocks.NEEDS_GOLD_TOOL);
-        disallowedBlocks.add(BlockTags.NEEDS_DIAMOND_TOOL);
-        disallowedBlocks.add(BlockTags.NEEDS_IRON_TOOL);
-    }
+    // This value shows how many ticks takes to look up a block in the digital miner's range
+    public static final int BASE_TICKS_PER_MINE = 10;
 }
--- src/main/java/io/github/yuko1101/digitalmineradjust/mixin/MixinTileEntityDigitalMiner.java
+++ src/main/java/io/github/yuko1101/digitalmineradjust/mixin/MixinTileEntityDigitalMiner.java
@@ -1,20 +1,50 @@
 package io.github.yuko1101.digitalmineradjust.mixin;
 
 import io.github.yuko1101.digitalmineradjust.DigitalMinerAdjust;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import mekanism.common.tile.machine.TileEntityDigitalMiner;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.BitSet;
+
 @Mixin(value = TileEntityDigitalMiner.class, remap = false)
-public class MixinTileEntityDigitalMiner {
-    @Inject(method = "canMine", at = @At("HEAD"), cancellable = true)
-    private void canMine(BlockState state, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        if (DigitalMinerAdjust.disallowedBlocks.stream().anyMatch(state::is)) {
-            cir.setReturnValue(false);
+public abstract class MixinTileEntityDigitalMiner {
+
+    @Shadow public abstract int getMaxY();
+
+    @Shadow public abstract int getMinY();
+
+    @Shadow public abstract int getRadius();
+
+    @Unique
+    private int digitalMinerAdjust$initialExpectedMineCount = 0;
+
+    @Inject(method = "getDelay", at = @At("HEAD"), cancellable = true)
+    private void getDelay(CallbackInfoReturnable<Integer> cir) {
+        if (digitalMinerAdjust$initialExpectedMineCount == 0) {
+            throw new IllegalStateException("");
         }
+        cir.setReturnValue(DigitalMinerAdjust.BASE_TICKS_PER_MINE * digitalMinerAdjust$rangeBlockCount() / digitalMinerAdjust$initialExpectedMineCount);
+    }
+
+    @Inject(method = "updateFromSearch", at = @At("HEAD"))
+    private void updateFromSearch(Long2ObjectMap<BitSet> oresToMine, int found, CallbackInfo ci) {
+        digitalMinerAdjust$initialExpectedMineCount = found;
+    }
+
+    @Inject(method = "reset", at = @At("HEAD"))
+    private void reset(CallbackInfo ci) {
+        digitalMinerAdjust$initialExpectedMineCount = 0;
+    }
+
+    @Unique
+    private int digitalMinerAdjust$rangeBlockCount() {
+        return (int) ((getMaxY() - getMinY() + 1) * Math.pow(getRadius() * 2.0 + 1, 2.0));
     }
 }
