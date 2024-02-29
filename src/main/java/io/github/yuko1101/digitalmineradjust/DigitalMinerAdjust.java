9849020c51671e434d59def69b8f626afe077cb6
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-02-29 18:40:29 +0900

    adjust delay

--- src/main/java/io/github/yuko1101/digitalmineradjust/DigitalMinerAdjust.java
+++ src/main/java/io/github/yuko1101/digitalmineradjust/DigitalMinerAdjust.java
@@ -7,5 +7,5 @@ public class DigitalMinerAdjust {
     public static final String MOD_ID = "digitalmineradjust";
 
     // This value shows how many ticks takes to look up a block in the digital miner's range
-    public static final int BASE_TICKS_PER_MINE = 10;
+    public static final int BASE_TICKS_PER_MINE = 20;
 }
--- src/main/java/io/github/yuko1101/digitalmineradjust/mixin/MixinTileEntityDigitalMiner.java
+++ src/main/java/io/github/yuko1101/digitalmineradjust/mixin/MixinTileEntityDigitalMiner.java
@@ -3,6 +3,7 @@ package io.github.yuko1101.digitalmineradjust.mixin;
 import io.github.yuko1101.digitalmineradjust.DigitalMinerAdjust;
 import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import mekanism.common.tile.machine.TileEntityDigitalMiner;
+import mekanism.common.util.MekanismUtils;
 import net.minecraft.core.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,16 +24,17 @@ public abstract class MixinTileEntityDigitalMiner {
 
     @Shadow public abstract int getRadius();
 
-    @Shadow private int cachedToMine;
     @Unique
     private int digitalMinerAdjust$initialExpectedMineCount = 0;
 
     @Inject(method = "getDelay", at = @At("HEAD"), cancellable = true)
     private void getDelay(CallbackInfoReturnable<Integer> cir) {
         if (digitalMinerAdjust$initialExpectedMineCount == 0) {
-            throw new IllegalStateException("");
+            throw new IllegalStateException("You can't call this method with initialExpectedMineCount 0.");
         }
-        cir.setReturnValue(DigitalMinerAdjust.BASE_TICKS_PER_MINE * digitalMinerAdjust$rangeBlockCount() / digitalMinerAdjust$initialExpectedMineCount);
+        final TileEntityDigitalMiner digitalMiner = ((TileEntityDigitalMiner)(Object) this);
+        final int delay = MekanismUtils.getTicks(digitalMiner, DigitalMinerAdjust.BASE_TICKS_PER_MINE * digitalMinerAdjust$rangeBlockCount() / digitalMinerAdjust$initialExpectedMineCount);
+        cir.setReturnValue(delay);
     }
 
     @Inject(method = "updateFromSearch", at = @At("HEAD"))
